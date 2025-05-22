7ddb46a6948e8f5a691425696fb4f32fe486d039
Asad_Ullah <au10050305@gmail.com>
2025-05-22 22:12:46 +0600

    get data from application.properties

--- README.md
+++ README.md
@@ -1,26 +1,72 @@
-# Pondit Portfolio
+# Portfolio
+Personal Portfolio Project using Java & Spring Boot for Pondits (Batch 7)
 
-A personal portfolio website showcasing projects and skills.
+## Learning Checklist
+- [x] Java 17
+- [ ] Core Concepts
+   - [x] Servlet
+   - [x] Convention over Configuration
+   - [x] Inversion of Control (IoC)
+   - [x] IoC Container
+   - [x] Dependency Injection
+   - [x] Application Context
+   - [x] Bean
+   - [x] DispatcherServlet
+   - [ ] Interceptor
+   - [ ] Filter
+- [x] Server-Side (using Template Engine)
+   - [x] / (Portfolio Homepage)
+   - [x] Practice Thymeleaf Tags (e.g. th:if, th:each, etc.)
+- [ ] REST API
+   - [X] /api/projects (CRUD for Projects)
+   - [ ] Secure Endpoints
+   - [ ] Input Validation
+   - [x] API Documentation: Swagger
+   - [ ] API Paging and Sorting
+- [x] application.properties
+   - [x] @ConfigurationProperties
+   - [x] @Value
+   - [x] yaml/yml format
+- [ ] Exception
+   - [x] Custom Exception Class
+   - [ ] Global Exception Handler
+- [x] Logging
+   - [x] Framework
+      - [x] Logback
+      - [x] Facade: SLF4J
+   - [x] Logging Levels
+   - [x] Configure Logging using application.properties
+   - [ ] Configure Logback using XML
+- [ ] External Service Call
+- [ ] Boilerplate Code Reduce
+   - [ ] Mapper for Mapping
+   - [ ] Lombok for Generating Getters and Setters
+- [ ] Spring Security
+   - [ ] Basic Auth
+   - [ ] JWT
+- [ ] Database
+   - [x] H2 (In-Memory Database)
+   - [ ] PostgreSQL
+- [x] Spring Profiles (local, dev, prod)
+- [ ] Deployment
+   - [x] Platform as-a Service (PaaS): [Render](https://portfolio-6nv7.onrender.com/)
+   - [ ] Infrastructure as-a-Service (IaaS): Virtual Machine
 
-## Features
-
-- Responsive design for desktop and mobile
-- Clean and modern UI
-- Sections for About, Projects, Skills, Contact
-- Easy to customize and extend
-
-## Technologies Used
-
-- HTML5
-- CSS3 (Tailwind CSS or Bootstrap, specify)
-- JavaScript (if any)
-
-## Getting Started
-
-### Installation
-
-1. Clone the repository
-
-   ```bash
-   git clone https://github.com/1005-AsadUllah/Pondit_portfolio.git
-   ```
+## How to Build & Run
+1. Install Java 17 (LTS) of any distribution. You can try Amazon Corretto 17. You can find the installation instructions here: https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/what-is-corretto-17.html
+2. Open this project with your preferred IDE. We prefer IntelliJ IDEA Ultimate Edition. Other IDEs like VS Code or Spring Tools Suite (STS) will do.
+3. To build the project execute the following command:
+    ```
+    ./mvnw clean package
+    ```
+   Executing that command will generate a jar under "target" folder which you can run using the following command:
+    ```
+    java -jar portfolio-0.0.1-SNAPSHOT.jar
+    ```
+   or, You can run the app directly by following the command:
+    ```
+    ./mvnw spring-boot:run
+    ```
+   or, you can run via your preferred IDE's RUN/PLAY button.
+4. After running the application, it will be up & running at http://localhost:8081
+5. You can also access the Swagger API Doc at: http://localhost:8081/swagger-ui/index.html# . You can go ahead and execute the Organization's REST APIs I prepared for you guys to play with.
--- src/main/java/com/pondit/demo/controllers/RootController.java
+++ src/main/java/com/pondit/demo/controllers/RootController.java
@@ -1,5 +1,7 @@
 package com.pondit.demo.controllers;
 
+import com.pondit.demo.config.ResumeConfig;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -10,127 +12,23 @@ import java.util.List;
 
 @Controller
 public class RootController {
-    static class PersonalInfo {
-        private String name;
-        private String email;
-        private String linkedinUrl;
-        private String githubUrl;
 
-        public PersonalInfo(String name, String email, String linkedinUrl, String githubUrl) {
-            this.name = name;
-            this.email = email;
-            this.linkedinUrl = linkedinUrl;
-            this.githubUrl = githubUrl;
-        }
 
-        // Getters
-        public String getName() {
-            return name;
-        }
+    private final ResumeConfig resumeConfig;
 
-        public String getEmail() {
-            return email;
-        }
-
-        public String getLinkedinUrl() {
-            return linkedinUrl;
-        }
-
-        public String getGithubUrl() {
-            return githubUrl;
-        }
-    }
-
-    static class Education {
-        private String degree;
-        private String institution;
-        private String years;
-        private String description;
-
-        public Education(String degree, String institution, String years, String description) {
-            this.degree = degree;
-            this.institution = institution;
-            this.years = years;
-            this.description = description;
-        }
-
-        // Getters
-        public String getDegree() {
-            return degree;
-        }
-
-        public String getInstitution() {
-            return institution;
-        }
-
-        public String getYears() {
-            return years;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-    }
-
-    static class Experience {
-        private String title;
-        private String company;
-        private String years;
-        private String description;
-
-        public Experience(String title, String company, String years, String description) {
-            this.title = title;
-            this.company = company;
-            this.years = years;
-            this.description = description;
-        }
-
-        // Getters
-        public String getTitle() {
-            return title;
-        }
-
-        public String getCompany() {
-            return company;
-        }
-
-        public String getYears() {
-            return years;
-        }
-
-        public String getDescription() {
-            return description;
-        }
+    public RootController(ResumeConfig resumeConfig) {
+        this.resumeConfig = resumeConfig;
     }
 
     @GetMapping
         public String indexPage (Model model)
 
         {
-            // Personal Info
-           PersonalInfo personalInfo = new PersonalInfo(
-                "Asad Ullah",
-                "asadmahmud211@gamil.com",
-                "https://www.linkedin.com/in/asad-ullah-0a0577291/",
-                "https://github.com/1005-AsadUllah"
-           );
 
-            // Education
-            List<Education> educationList = Arrays.asList(
-                new Education("Diploma in Computer Science and Technology", "Bangladesh Sweden Polytechnic Institute", "2021-2025", "GPA: 3.8/4.0"),
-                new Education("SSC", "Abutorab High School", "2016 - 2020", "GPA: 4.83/5.0")
-            );
-
-            // Experience
-            List<Experience> experienceList = Arrays.asList(
-                new Experience("Software Engineer Intern", "Tech Company A", "June 2022 - August 2022", "Worked on developing web applications using Java and Spring Boot."),
-                new Experience("Freelance Developer", "Self-Employed", "January 2021 - May 2022", "Developed various projects including a personal portfolio website.")
-            );
-            List<String> skills = Arrays.asList("Java", "Spring Boot", "Microservices", "MySQL", "Thymeleaf", "Tailwind CSS");
-            model.addAttribute("educationList", educationList);
-            model.addAttribute("experienceList", experienceList);
-           model.addAttribute("personalInfo", personalInfo);
-            model.addAttribute("skills", skills);
+            model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
+            model.addAttribute("educationList", resumeConfig.getEducation());
+           model.addAttribute("experienceList", resumeConfig.getExperience());
+            model.addAttribute("skills", resumeConfig.getSkills());
 
             return "home";
         }
